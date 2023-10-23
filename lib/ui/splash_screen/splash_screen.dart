53024c7bed17f39d99bc243e3c9bab8a4ffa295b
Ammar-Yasser <mathn.ammaryasser@gmail.com>
2023-10-23 20:18:45 +0300

    Created splash screen

--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -6,7 +6,43 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Self Care",
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/logo.png"))),
+                  )
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Text(
+                "Loading...",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium
+                    ?.copyWith(color: Colors.grey),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
